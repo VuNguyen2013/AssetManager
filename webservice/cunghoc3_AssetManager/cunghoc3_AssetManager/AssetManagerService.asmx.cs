97ed06a10af8ead778d3c6f810e7e2128e6dce67
stankovic2503 <kukumalu2503@gmail.com>
2013-07-29 12:54:16 +0700

    Complete basics funtions on Web Service

--- webservice/cunghoc3_AssetManager/cunghoc3_AssetManager/AssetManagerService.asmx.cs
+++ webservice/cunghoc3_AssetManager/cunghoc3_AssetManager/AssetManagerService.asmx.cs
@@ -62,7 +62,7 @@ namespace cunghoc3_AssetManager
             AssetGroupType item = db.GetById(id);
             item.Name = name;
             db.Update(item);
-            return "Update new successful";
+            return "Update item successful";
         }
 
         [WebMethod]
@@ -104,14 +104,14 @@ namespace cunghoc3_AssetManager
         }
 
         [WebMethod]
-        public string UpdateAssetGroupType(string id, string Name, string AssetGroupTypeId)
+        public string UpdateAssetGroup(string id, string Name, string AssetGroupTypeId)
         {
             cunghoc3_AssetManager.Services.AssetGroupService db = new Services.AssetGroupService();
             AssetGroup item = db.GetById(id);
             item.Name = Name;
             item.AssetGroupTypeId = AssetGroupTypeId;
             db.Update(item);
-            return "Update new successful";
+            return "Update item successful";
         }
 
         [WebMethod]
@@ -128,6 +128,8 @@ namespace cunghoc3_AssetManager
             } while (!db.Insert(item));
             return "Add new Capital successful";
         }
+
+
 
         [WebMethod]
         public List<Capital> GetAllCapitalGroup()
@@ -153,14 +155,14 @@ namespace cunghoc3_AssetManager
         }
 
         [WebMethod]
-        public string UpdateCapitalType(string id, string Name, string Note)
+        public string UpdateCapital(string id, string Name, string Note)
         {
             cunghoc3_AssetManager.Services.CapitalService db = new Services.CapitalService();
             Capital item = db.GetById(id);
             item.Name = Name;
             item.Note = Note;
             db.Update(item);
-            return "Update new successful";
+            return "Update item successful";
         }
 
         [WebMethod]
@@ -181,6 +183,42 @@ namespace cunghoc3_AssetManager
         }
 
         [WebMethod]
+        public string UpdateDepartmentUsed(string id, string Name, string Phone, string Representative, string Address)
+        {
+            cunghoc3_AssetManager.Services.DepartmentUsedService db = new Services.DepartmentUsedService();
+            DepartmentUsed item = db.GetById(id);
+            item.Name = Name;
+            item.Phone = Phone;
+            item.Representative = Representative;
+            item.Address = Address;
+            db.Update(item);
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<DepartmentUsed> GetDepartmentUsed()
+        {
+            cunghoc3_AssetManager.Services.DepartmentUsedService db = new Services.DepartmentUsedService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public DepartmentUsed GetDepartmentUsedById(string id)
+        {
+            cunghoc3_AssetManager.Services.DepartmentUsedService db = new Services.DepartmentUsedService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelDepartmentUsedById(string id)
+        {
+            cunghoc3_AssetManager.Services.DepartmentUsedService db = new Services.DepartmentUsedService();
+            DepartmentUsed item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
+        }
+
+        [WebMethod]
         public string NewUnit(string Name, string Note)
         {
             cunghoc3_AssetManager.Services.UnitService db = new Services.UnitService();
@@ -193,6 +231,40 @@ namespace cunghoc3_AssetManager
                 item.Note = Note;
             } while (!db.Insert(item));
             return "Add new Unit successful";
+        }
+
+        [WebMethod]
+        public string UpdateUnit(string id, string Name, string Note)
+        {
+            cunghoc3_AssetManager.Services.UnitService db = new Services.UnitService();
+            Unit item = db.GetById(id);
+            item.Name = Name;
+            item.Note = Note;
+            db.Update(item);
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<Unit> GetAllUnit()
+        {
+            cunghoc3_AssetManager.Services.UnitService db = new Services.UnitService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public Unit GetUnitById(string id)
+        {
+            cunghoc3_AssetManager.Services.UnitService db = new Services.UnitService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelUnitById(string id)
+        {
+            cunghoc3_AssetManager.Services.UnitService db = new Services.UnitService();
+            Unit item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
         }
 
         [WebMethod]
@@ -231,6 +303,60 @@ namespace cunghoc3_AssetManager
         }
 
         [WebMethod]
+        public string UpdateAsset(string id, string Name, string AssetGroupId, string UnitId, string Amount, string CounPro, string YearPro, string DepartmentUsedId, string TotalPrice, string BugetPrice, string OwnPrice, string VenturePrice, string AnotherPrice, string TotalDepreciation, string BugetDepreciation, string OwnDepreciation, string VentureDepreciation, string AnotherDepreciation, string BugeRemain, string OwnRemain, string VentureRemain, string AnotherRemain, string TotalRemain, string UpDownCode, string InputDateTime)
+        {
+            cunghoc3_AssetManager.Services.AssetService db = new Services.AssetService();
+            Asset item = db.GetById(id);
+            item.Name = Name;
+            item.AssetGroupId = AssetGroupId;
+            item.UnitId = UnitId;
+            item.Amount = Convert.ToInt32(Amount);
+            item.CounPro = CounPro;
+            item.YearPro = Convert.ToInt32(YearPro);
+            item.DepartmentUsedId = DepartmentUsedId;
+            item.TotalPrice = Convert.ToInt64(TotalPrice);
+            item.BudgetPrice = Convert.ToInt64(BugetPrice);
+            item.OwnPrice = Convert.ToInt64(OwnPrice);
+            item.VenturePrice = Convert.ToInt64(VenturePrice);
+            item.AnotherPrice = Convert.ToInt64(AnotherPrice);
+            item.TotalDepreciation = Convert.ToInt64(TotalDepreciation);
+            item.BudgetDepreciation = Convert.ToInt64(BugetDepreciation);
+            item.OwnDepreciation = Convert.ToInt64(OwnDepreciation);
+            item.VentureDepreciation = Convert.ToInt64(VentureDepreciation);
+            item.AnotherDepreciation = Convert.ToInt64(AnotherDepreciation);
+            item.BudgetRemain = Convert.ToInt64(BugeRemain);
+            item.OwnRemain = Convert.ToInt64(OwnRemain);
+            item.VentureRemain = Convert.ToInt64(VentureRemain);
+            item.AnotherRemain = Convert.ToInt64(AnotherRemain);
+            item.TotalReamain = Convert.ToInt64(TotalRemain);
+            db.Update(item);
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<Asset> GetAllAsset()
+        {
+            cunghoc3_AssetManager.Services.AssetService db = new Services.AssetService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public Asset GetAssetById(string id)
+        {
+            cunghoc3_AssetManager.Services.AssetService db = new Services.AssetService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelAssetById(string id)
+        {
+            cunghoc3_AssetManager.Services.AssetService db = new Services.AssetService();
+            Asset item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
+        }
+
+        [WebMethod]
         public string NewPartner(string Name, string Phone, string TaxCode, string Address)
         {
             cunghoc3_AssetManager.Services.PartnerService db = new Services.PartnerService();
@@ -245,6 +371,42 @@ namespace cunghoc3_AssetManager
                 item.TaxCode = TaxCode;
             } while (db.Insert(item));
             return "Add new Partner successful";
+        }
+
+        [WebMethod]
+        public string UpdatePartner(string id, string Name, string Phone, string TaxCode, string Address)
+        {
+            cunghoc3_AssetManager.Services.PartnerService db = new Services.PartnerService();
+            Partner item = db.GetById(id);
+            item.Name = Name;
+            item.Phone = Phone;
+            item.Address = Address;
+            item.TaxCode = TaxCode;
+            db.Update(item);
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<Partner> GetAllPartner()
+        {
+            cunghoc3_AssetManager.Services.PartnerService db = new Services.PartnerService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public Partner GetPartnerById(string id)
+        {
+            cunghoc3_AssetManager.Services.PartnerService db = new Services.PartnerService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelPartnerById(string id)
+        {
+            cunghoc3_AssetManager.Services.PartnerService db = new Services.PartnerService();
+            Partner item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
         }
 
         [WebMethod]
@@ -263,6 +425,40 @@ namespace cunghoc3_AssetManager
         }
 
         [WebMethod]
+        public string UpdateUpDownReason(string id, string Name, string Type)
+        {
+            cunghoc3_AssetManager.Services.UpDownReasonService db = new Services.UpDownReasonService();
+            UpDownReason item = db.GetById(id);
+            item.Name = Name;
+            item.Type = Type;
+            db.Update(item);
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<UpDownReason> GetAllUpDownReason()
+        {
+            cunghoc3_AssetManager.Services.UpDownReasonService db = new Services.UpDownReasonService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public UpDownReason GetUpDownReasonById(string id)
+        {
+            cunghoc3_AssetManager.Services.UpDownReasonService db = new Services.UpDownReasonService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelUpDownReasonById(string id)
+        {
+            cunghoc3_AssetManager.Services.UpDownReasonService db = new Services.UpDownReasonService();
+            UpDownReason item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
+        }
+
+        [WebMethod]
         public string NewAssetLiquidation(string AssetId, string DepartmentUsedId, string LiDateTime, string LiPrice)
         {
             cunghoc3_AssetManager.Services.AssetLiquidationService db = new Services.AssetLiquidationService();
@@ -277,6 +473,42 @@ namespace cunghoc3_AssetManager
                 item.LiPrice = Convert.ToInt64(LiPrice);
             } while (!db.Insert(item));
             return "Add new AssetLiquidation successful";
+        }
+
+        [WebMethod]
+        public string UpdateAssetLiquidation(string id, string AssetId, string DepartmentUsedId, string LiDateTime, string LiPrice)
+        {
+            cunghoc3_AssetManager.Services.AssetLiquidationService db = new Services.AssetLiquidationService();
+            AssetLiquidation item = db.GetById(id);
+            item.AssetId = AssetId;
+            item.DepartmentUsedId = DepartmentUsedId;
+            item.LiDateTime = DateTime.Parse(LiDateTime);
+            item.LiPrice = Convert.ToInt64(LiPrice);
+            db.Update(item);
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<AssetLiquidation> GetAllAssetLiquidation()
+        {
+            cunghoc3_AssetManager.Services.AssetLiquidationService db = new Services.AssetLiquidationService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public AssetLiquidation GetAssetLiquidationById(string id)
+        {
+            cunghoc3_AssetManager.Services.AssetLiquidationService db = new Services.AssetLiquidationService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelAssetLiquidationById(string id)
+        {
+            cunghoc3_AssetManager.Services.AssetLiquidationService db = new Services.AssetLiquidationService();
+            AssetLiquidation item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
         }
 
         [WebMethod]
@@ -299,6 +531,44 @@ namespace cunghoc3_AssetManager
         }
 
         [WebMethod]
+        public string UpdateRepairAsset(string id, string AssetId, string DepartmentUsedId, string PartnerId, string Address, string RepairDate, string Fee)
+        {
+            cunghoc3_AssetManager.Services.RepairAssetService db = new Services.RepairAssetService();
+            RepairAsset item = db.GetById(id);
+            item.AssetId = AssetId;
+            item.DepartmentUsedId = DepartmentUsedId;
+            item.PartnerId = PartnerId;
+            item.Address = Address;
+            item.RepairDate = DateTime.Parse(RepairDate);
+            item.Fee = Convert.ToInt64(Fee);
+            db.Update(item);
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<RepairAsset> GetAllRepairAsset()
+        {
+            cunghoc3_AssetManager.Services.RepairAssetService db = new Services.RepairAssetService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public RepairAsset GetRepairAssetById(string id)
+        {
+            cunghoc3_AssetManager.Services.RepairAssetService db = new Services.RepairAssetService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelRepairAssetById(string id)
+        {
+            cunghoc3_AssetManager.Services.RepairAssetService db = new Services.RepairAssetService();
+            RepairAsset item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
+        }
+
+        [WebMethod]
         public string NewWarrantyAsset(string AssetId, string DepartmentUsedId, string PartnerId, string WarDateTime, string DeadlineWar, string Address, string PersonWar)
         {
             cunghoc3_AssetManager.Services.WarrantyAssetService db = new Services.WarrantyAssetService();
@@ -317,5 +587,44 @@ namespace cunghoc3_AssetManager
             } while (!db.Insert(item));
             return "Add new WarrantyAsset successful";
         }
+
+        [WebMethod]
+        public string UpdateWarrantyAsset(string id, string AssetId, string DepartmentUsedId, string PartnerId, string WarDateTime, string DeadlineWar, string Address, string PersonWar)
+        {
+            cunghoc3_AssetManager.Services.WarrantyAssetService db = new Services.WarrantyAssetService();
+            WarrantyAsset item = db.GetById(id);
+            item.AsssetId = AssetId;
+            item.DepartmentUsedId = DepartmentUsedId;
+            item.PartnerId = PartnerId;
+            item.WarDateTime = DateTime.Parse(WarDateTime);
+            item.DeadlineWar = DateTime.Parse(DeadlineWar);
+            item.Address = Address;
+            item.PersonWar = PersonWar;
+            return "Update item successful";
+        }
+
+        [WebMethod]
+        public List<WarrantyAsset> GetAllWarrantyAsset()
+        {
+            cunghoc3_AssetManager.Services.WarrantyAssetService db = new Services.WarrantyAssetService();
+            return db.GetAll().ToList();
+        }
+
+        [WebMethod]
+        public WarrantyAsset GetWarrantyAssetById(string id)
+        {
+            cunghoc3_AssetManager.Services.WarrantyAssetService db = new Services.WarrantyAssetService();
+            return db.GetById(id);
+        }
+
+        [WebMethod]
+        public string DelWarrantyAssetById(string id)
+        {
+            cunghoc3_AssetManager.Services.WarrantyAssetService db = new Services.WarrantyAssetService();
+            WarrantyAsset item = db.GetById(id);
+            db.Delete(item);
+            return "Delete item successful";
+        }
+
     }
 }
