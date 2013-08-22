cf9e6f42894689361e5cab06ee0f7609f53e78b3
stankovic2503 <kukumalu2503@gmail.com>
2013-08-22 16:56:09 +0700

    commit

---
+++ webservice/cunghoc3_AssetManager/cunghoc3_AssetManager/DataContract/RepairAssetData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cunghoc3_AssetManager.DataContract
+{
+    public class RepairAssetData
+    {
+        public string Id { get; set; }
+        public string AssetName { get; set; }
+        public string DepartmentUsedName { get; set; }
+        public string PartnerName { get; set; }
+        public string Address { get; set; }
+        public DateTime RepairDate { get; set; }
+        public long Fee { get; set; }
+    }
+}
